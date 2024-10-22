2e3f0f7f3b18018c7ebc17e305e9e534a28cefea
Jsims0 <jd.sims410@gmail.com>
2024-10-22 15:20:40 +0100

    inntial commit with planned layout

---
+++ main.py
@@ -0,0 +1,25 @@
+#imports
+
+#functions
+
+##dx login
+
+##find tar files
+
+##output tar files
+
+##delete tar files
+
+##check date
+
+##get date for deletion(6 months ago)
+
+
+#inputs
+## argumets or read from config?
+
+#get/check credetials
+
+#get old tar files
+
+#record files for deletion
