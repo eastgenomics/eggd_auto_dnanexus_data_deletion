a70c8ecd02cc0b4fbdcc6293ed51d0c28bf0e63c
Jsims0 <jd.sims410@gmail.com>
2024-10-23 12:39:47 +0100

    added dx login methods

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 #imports
+import dxpy as dx
+import logging
+import os
+import sys
 
 #functions
 
 ##dx login
+def get_credentials(path):
+    with open(f'{path}','r') as file:
+        AUTH_TOKEN =file.read().rstrip()
+    
+    return AUTH_TOKEN
+
+def dx_login(token):
+    """
+    Function to check authenticating to DNAneuxs
+
+    Parameters
+    ----------
+    token : str
+        DNAnexus authentication token
+    """
+    try:
+        DX_SECURITY_CONTEXT = {
+            "auth_token_type": "Bearer",
+            "auth_token": str(token)
+        }
+
+        dx.set_security_context(DX_SECURITY_CONTEXT)
+        print(dx.api.system_whoami())
+    except dx.exceptions.InvalidAuthentication as err:
+        print(err)        
 
 ##find tar files
 
-##output tar files
+##output tar file details
 
 ##delete tar files
 
@@ -19,7 +48,19 @@
 ## argumets or read from config?
 
 #get/check credetials
+def main ():
+    print(sys.argv[1])
+    auth_token = get_credentials(sys.argv[1])
+
+    dx_login(auth_token)
+
+
+
+    
 
 #get old tar files
 
 #record files for deletion
+
+if __name__=='__main__':
+    main()
