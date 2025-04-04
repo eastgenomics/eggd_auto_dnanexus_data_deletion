a57ac82ef1b25fc6f46a5f0d91800ec38b3d0bdf
Jsims0 <jd.sims410@gmail.com>
2025-04-04 11:28:35 +0100

    fix output not being found for upload

--- src/AutoDelete.sh
+++ src/AutoDelete.sh
@@ -14,8 +14,8 @@ main() {
 
     python3 /home/dnanexus/eggd_automatic_deletion/main.py --config config_file $project 
 
-
-if [[ -s /home/dnanexus/*_files_to_delete_*.csv ]]; then
+files=(/home/dnanexus/*_files_to_delete_*.csv)
+if [[ -s "${files[0]}" ]]; then
     output_file=$(dx upload /home/dnanexus/*_files_to_delete_*.csv --brief)
 
     dx-jobutil-add-output output_file "$output_file" --class=file
